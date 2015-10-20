43241e31f3565ced042595e44fe4b7a35d906233
chafik Aboulaz <CAboulaz@us.loreal.com>
2015-10-20 09:56:53 -0400

    test commit1

--- desktop/src/com/mygdx/libgdxtuto/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/libgdxtuto/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.libgdxtuto.LibgdxTuto;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 500;
+		config.height = 250;
 		new LwjglApplication(new LibgdxTuto(), config);
 	}
 }
