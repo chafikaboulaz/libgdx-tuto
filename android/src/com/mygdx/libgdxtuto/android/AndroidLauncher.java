fd1c2f2fbd17fc9756a9a33148e08a05b025939d
chafik Aboulaz <CAboulaz@us.loreal.com>
2015-10-20 09:50:32 -0400

    init

---
+++ android/src/com/mygdx/libgdxtuto/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.libgdxtuto.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.libgdxtuto.LibgdxTuto;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LibgdxTuto(), config);
+	}
+}
